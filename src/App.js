799ed0ffbed0f1c2543dd23632e71d827bb64534
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-09-09 23:16:49 -0400

    ! Working with a basic structure for header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import AppHeader from './components/AppHeader'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hola Mundo!</h1>
+      <AppHeader />
     </div>
   );
 }
---
+++ src/components/AppHeader/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AppHeader() {
+    return <header>
+        hola mundo!!!!!!!!!
+    </header>
+}
