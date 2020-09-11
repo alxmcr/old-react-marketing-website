9c8f655e1677749bbf949e91fb63a062c94985a2
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-09-10 23:26:41 -0400

    ! Working with header

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -3,9 +3,13 @@ import "./AppHeader.css";
 
 export default function AppHeader() {
     return <header>
-        <h1 className="header__title">
-            <span className="header__spotlight--black">UNIVERSAL</span> 
-            <span className="header__spotlight--white">Headphones</span>
+        <h1 className="header__spotlight">
+            <span className="spotlight__text spotlight__text--black">UNIVERSAL</span>
+            <span className="spotlight__text spotlight__text--white">Headphones</span>
         </h1>
+        <div className="header__actions">
+            <a href="#" className="header__link header__link--info">Buy for $ 68.99</a>
+            <a href="#register" className="header__link header__link--success">Register</a>
+        </div>
     </header>
 }
