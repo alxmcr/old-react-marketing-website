08adb9546bb04a70a9dd415cb9cda4dfbbf619d7
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-09-12 23:07:08 -0400

    * Fix React warnings

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -8,7 +8,7 @@ export default function AppHeader() {
             <span className="spotlight__text spotlight__text--white">Headphones</span>
         </h1>
         <div className="header__actions">
-            <a href="#" onClick="" className="header__link header__link--info">Buy for $ 68.99</a>
+            <a href="/" className="header__link header__link--info">Buy for $ 68.99</a>
             <a href="#register" className="header__link header__link--success">Register</a>
         </div>
     </header>
