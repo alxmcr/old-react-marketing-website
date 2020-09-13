31c785bfb543157f69204b3a8c342325804691c7
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-09-12 23:12:56 -0400

    + Add link to Github's repository

--- src/components/AppFooter/index.js
+++ src/components/AppFooter/index.js
@@ -1,14 +1,26 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
 import "./AppFooter.css"
 
 export default function AppFooter() {
     return <footer>
-        <span className="footer__text">Made with</span>
-        <span className="footer__icon">
-            <FontAwesomeIcon icon={faHeart} />
-        </span>
-        <span className="footer__text">in Latam</span>
+        <div className="footer__row">
+            <span className="footer__text">Made with</span>
+            <span className="footer__icon">
+                <FontAwesomeIcon icon={faHeart} />
+            </span>
+            <span className="footer__text">in Latam</span>
+        </div>
+        <div className="footer__row footer__row--repository">
+            <a href="https://github.com/alxmcr/react-marketing-website" className="footer__link">
+                <span className="footer__text">Code</span>
+                <span className="footer__icon">
+                    <FontAwesomeIcon icon={faGithub} />
+                </span>
+            </a>
+        </div>
     </footer>
 }
